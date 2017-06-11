ccdbdfd44fb02324eb3b775fb5701d78b52cb397
Roman Levytskyy <r.levyts@gmail.com>
2017-06-11 18:59:54 -0400

    make both ios and android render app component

---
+++ components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import HomePage from './homePage'
+import {
+  Navigator
+} from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <HomePage/>
+    );
+  }
+}
