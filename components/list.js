b47070a25675dabe54f6d228079f718335016814
Roman Levytskyy <r.levyts@gmail.com>
2017-06-15 20:31:35 -0400

    list component renders a list of items

--- components/list.js
+++ components/list.js
@@ -2,17 +2,28 @@ import React from 'react';
 import {
   StyleSheet,
   Text,
-  View
+  View,
+  TextInput,
+  TouchableOpacity
 } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 
 
 export default class List extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      todos: ['a', 'b', 'c', 'd'],
+      newTodo: ''
+    }
+  }
+
   render() {
-    debugger
     return (
       <View style={styles.container}>
-        <Text>{this.props.navigation.state.params.username}</Text>
+        {this.state.todos.map((todo, i) => {
+          return <Text key={i}>{todo}</Text>
+        })}
       </View>
     );
   }
