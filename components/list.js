d658a56e0c1d1d55ca6db1f3269f2fb3689c1fb2
Roman Levytskyy <r.levyts@gmail.com>
2017-06-15 20:32:56 -0400

    add functionality to add todo items

--- components/list.js
+++ components/list.js
@@ -18,9 +18,22 @@ export default class List extends React.Component {
     }
   }
 
+  addTodo(){
+    const todos = [...this.state.todos, this.state.newTodo]
+    this.setState({todos, newTodo: ''})
+  }
+
   render() {
     return (
       <View style={styles.container}>
+        <TextInput
+          style={styles.input}
+          onChangeText={newTodo => this.setState({newTodo})}
+          value={this.state.newTodo}
+        />
+        <TouchableOpacity onPress={this.addTodo.bind(this)}>
+          <Text>Click me</Text>
+        </TouchableOpacity>
         {this.state.todos.map((todo, i) => {
           return <Text key={i}>{todo}</Text>
         })}
@@ -35,5 +48,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    height: 20,
+    width: 200,
+    borderColor: 'orange',
+    borderWidth: 2
   }
 });
